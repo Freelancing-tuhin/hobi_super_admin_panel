72564fbaf59f1c8463a899a434df5983cd7e1a27
Tuhin810 <tuhin.thakur1233@gmail.com>
2025-02-27 21:48:01 +0530

    Update welcome messages and improve code formatting in WelcomeBox and WelcomeBanner components

--- src/components/dashboards/Dashboard1/WelcomeBox.tsx
+++ src/components/dashboards/Dashboard1/WelcomeBox.tsx
@@ -1,8 +1,6 @@
-
-import CardBox from "../../shared/CardBox";
-import { Icon } from "@iconify/react";
+import CardBox from '../../shared/CardBox';
+import { Icon } from '@iconify/react';
 import Background from '/src/assets/images/backgrounds/welcome-bg.png';
-
 
 const WelcomeBox = () => {
   return (
@@ -12,13 +10,9 @@ const WelcomeBox = () => {
           <div className="md:col-span-7 col-span-12">
             <div className="flex gap-4 items-center">
               <div className="h-12 w-12 rounded-tw bg-white flex items-center justify-center flex-shrink-0 ">
-                <Icon
-                  icon="solar:course-up-outline"
-                  className="text-dark opacity-70"
-                  height={24}
-                />
+                <Icon icon="solar:course-up-outline" className="text-dark opacity-70" height={24} />
               </div>
-              <h5 className="text-xl text-white">Welcome Back David</h5>
+              <h5 className="text-xl text-white">Welcome Back Tuhin</h5>
             </div>
 
             <div className="flex  w-full xl:mt-12 sm:mt-12 lg:mt-6 mt-6">
@@ -26,18 +20,18 @@ const WelcomeBox = () => {
                 <p className="text-white opacity-75 text-sm mb-1">Budget</p>
                 <h2 className="text-white text-2xl">$98,450</h2>
               </div>
-              <div className="ps-4"> 
+              <div className="ps-4">
                 <p className="text-white opacity-75 text-sm mb-1">Budget</p>
                 <h2 className="text-white text-2xl">$2,440</h2>
               </div>
             </div>
           </div>
           <div className="md:col-span-5 col-span-12 md:ms-auto ms-auto me-auto">
-          <img
-          src={Background}
-          alt="background"
-          className="-mb-n5 rtl:scale-x-[-1] xl:max-w-[170px] lg:max-w-36 md:max-w-36 max-w-32 lg:ps-4 md:pt-0 pt-6"
-        />
+            <img
+              src={Background}
+              alt="background"
+              className="-mb-n5 rtl:scale-x-[-1] xl:max-w-[170px] lg:max-w-36 md:max-w-36 max-w-32 lg:ps-4 md:pt-0 pt-6"
+            />
           </div>
         </div>
       </CardBox>
--- src/components/widgets/banners/WelcomeBanner.tsx
+++ src/components/widgets/banners/WelcomeBanner.tsx
@@ -1,7 +1,6 @@
-
-import { Button } from "flowbite-react";
-import Banner from "/src/assets/images/backgrounds/welcome-bg2.png";
-import CardBox from "src/components/shared/CardBox";
+import { Button } from 'flowbite-react';
+import Banner from '/src/assets/images/backgrounds/welcome-bg2.png';
+import CardBox from 'src/components/shared/CardBox';
 
 const WelcomeBanner = () => {
   return (
@@ -9,7 +8,7 @@ const WelcomeBanner = () => {
       <CardBox className="bg-primary-gt shadow-none pb-0 pe-0">
         <div className="grid grid-cols-12 gap-[30px]">
           <div className="md:col-span-6 col-span-12">
-            <h5 className="text-lg text-white mt-2">Welcome back David!</h5>
+            <h5 className="text-lg text-white mt-2">Welcome back Tuhin!</h5>
             <p className="text-white opacity-75 text-sm font-medium py-5">
               You have earned 54% more than last month which is great thing.
             </p>
