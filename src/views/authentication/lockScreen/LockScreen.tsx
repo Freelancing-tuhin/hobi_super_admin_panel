defe1a33c4d39ba5675d02d1039c1387df7d747a
Tuhin810 <tuhin.thakur1233@gmail.com>
2025-03-01 01:38:49 +0530

    Enhance LockScreen component to check for required user documents and verification status

--- src/views/authentication/lockScreen/LockScreen.tsx
+++ src/views/authentication/lockScreen/LockScreen.tsx
@@ -2,19 +2,38 @@ import { useContext } from 'react';
 import { Link } from 'react-router';
 import { AuthContext } from 'src/context/authContext/AuthContext';
 
+const REQUIRED_FIELDS = [
+  'licenses_for_establishment',
+  'certificate_of_incorporation',
+  'licenses_for_activity_undertaken',
+  'certifications',
+  'insurance_for_outdoor_activities',
+  'health_safety_documents',
+]; // Add any required fields here
+
 const LockScreen = () => {
   const { user }: any = useContext(AuthContext);
+
+  const hasAllRequiredFields = REQUIRED_FIELDS.every((field) => user?.[field]);
+
+  if (hasAllRequiredFields && user?.is_verified) {
+    return null; // Don't show LockScreen if user is verified
+  }
 
   return (
     <div className="fixed inset-0 bg-black/50 backdrop-blur-md flex items-center justify-center z-50">
       {/* Lock Icon and Message */}
       <div className="flex flex-col items-center bg-black/60 p-6 rounded-xl shadow-lg">
-        {/* <LockKey size={80} weight="bold" className="text-white mb-4" /> */}
-        <p className="text-xl font-semibold text-white">🔒</p>
-        <p className="text-xl font-semibold text-white">This content is locked</p>
-        <p className="text-sm text-gray-300">
-          Please upload your documents to access all features.
-        </p>
+        <p className="text-xl font-semibold text-white mb-2">This content is locked</p>
+        {/* Show appropriate message based on user status */}
+        {!hasAllRequiredFields ? (
+          <p className="text-sm text-gray-300">
+            {' '}
+            Please upload your documents to access all features.
+          </p>
+        ) : !user?.is_verified ? (
+          <p className="text-sm text-gray-300">Please wait until your account is verified.</p>
+        ) : null}
         <Link
           to={'/apps/user-profile/profile'}
           className="bg-blue-500 text-white py-2 px-4 mt-3 rounded cursor-pointer"
